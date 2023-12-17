7c1204571604afee8cb3493d33a464d5029420ea
Arup Roy <aruproy5637@gmail.com>
2023-12-17 19:51:18 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// 1. NAN
+let score = 33;
+console.log(typeof score);
+
+let age = "12abc";
+console.log(typeof age);
+
+let valueInNumber = Number(age);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33, "33abc" => NaN, true => 1, false => 0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false, "" => false, "Arup" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
+
+
+
