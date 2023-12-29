220076f29168fcdcefbfb1b134c6433d01292d87
Arup Roy <aruproy5637@gmail.com>
2023-12-29 17:54:47 +0000

    Why string to number conversion is confusing

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,6 +23,62 @@ let stringNumber = String(someNumber);
 console.log(typeof stringNumber);
 console.log(stringNumber);
 
+// ************* Operations **********
+let value = 3;
+let negValue = -value;
+console.log(value);
+console.log(negValue);
+
+// basic math
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(4/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "Arup"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+// complex thing now
+// If STRING come first then = whole string
+// If some number are there then add those number added then string
+console.log("1" + 2); // string and number = string 12
+console.log(1 + "2"); // number and string = string 12 
+console.log("2" + 2 + 2);  // string 222
+console.log(2 + 2 + "1");  // string but 41
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+// prefix and postfix to study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
+
+
+
+
+
 
 
 
