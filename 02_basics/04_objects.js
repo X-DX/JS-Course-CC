de3bf9fd728f20cd429abd927bead86d70974ab7
Arup Roy <aruproy5637@gmail.com>
2024-01-07 00:40:59 +0000

    obj 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,13 @@
+// constructor
+
+// declear Objects (same way to declear)
+// const tinderUser = new Object()
+const tinderUser = {}
+
+// console.log(tinderUser);
+
+tinderUser.id = "1234Abc"
+tinderUser.name = "Arup"
+tinderUser.isLoggedIn = false
+// console.log(tinderUser);
+
