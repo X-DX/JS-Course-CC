c86b4980d5a37b90534d784fddfc1cf72d7d2519
Arup Roy <aruproy5637@gmail.com>
2024-01-07 07:06:26 +0000

    obj

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -11,3 +11,49 @@ tinderUser.name = "Arup"
 tinderUser.isLoggedIn = false
 // console.log(tinderUser);
 
+const regularUser = {
+    email: "test@gmail.com",
+    fullname: {
+        userFullName: {
+            fisrtName: "Arup",
+            lastName: "Roy"
+        }
+    }
+}
+// console.log(regularUser);
+// console.log(regularUser.fullname.userFullName.fisrtName);
+
+const obj1 = {
+    1: "a",
+    2: "b"
+}
+const obj2 = {
+    3: "x",
+    4: "y"
+}
+
+// Study here: 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+
+// const obj3 = Object.assign({}, obj1, obj2) // target {}, and other are source
+const obj3 = {...obj1, ...obj2} // same this using spread
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "test@gmail.com"
+    },
+    {
+        name: "Arup"
+    },
+    {
+        Course: "Course"
+    }
+]
+users[1].email
+console.log(users);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
