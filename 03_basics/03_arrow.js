f4e77d77ff6bfd6042975ca716a9885692d5f304
Arup Roy <aruproy5637@gmail.com>
2024-01-14 12:05:44 +0000

    arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,65 @@
+// arrror function
+// 1. this -> current context
+const user = {
+    username: "hitest",
+    price: 900,
+
+    welcomeMessage: function() {
+        console.log(`${this.username}, welcome to website`);
+        // console.log(this);  // tell about current context or current value
+    }
+}
+// user.welcomeMessage
+// user.welcomeMessage()
+// user.username="Arup"
+// user.welcomeMessage()
+
+// console.log(this);  // empty {}, node enironment 
+
+// In JavaScript, the behavior of this in the global context depends on where the code is executed. If the code is executed in the global scope, this refers to the global object, which is window in a browser environment.
+
+// However, when you use console.log(this) in certain environments like a browser's console, the output may appear as {} even though it represents the global object (window in a browser). This is a common representation used by some console environments to indicate the global object.
+
+
+
+// function chai(){
+//     let username = "hitesh"
+//     // console.log(this);
+//     console.log(this.username);
+// }
+
+// chai()
+
+// 2. arrow function
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+// // chai()
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+// chai()
+
+// explesite return
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+
+//3. implesite return (use in react)
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+// const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
