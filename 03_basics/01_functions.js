78f1804aa90ad907dad7ff81851489e81dde55c5
X-DX <aruproy5637@gmail.com>
2024-04-13 12:45:19 +0530

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,3 +1,5 @@
+//! Function
+
 function sayMyName(){
     console.log("H");
     console.log("I");
@@ -7,28 +9,35 @@ function sayMyName(){
     console.log("H");
 }
 
-// sayMyName -> reference
-//  sayMyName() -> its call excute
+// sayMyName //? reference
+//  sayMyName() //? call execution
 
-// 1. call function with arguments and return value
+//! 1. call function with arguments and return value
 
-// function addTwoNumbers(number1, number2){
-
+// function addTwoNumbers(number1, number2){ //? parameters (number1, number2)
 //     console.log(number1 + number2);
 // }
+// addTwoNumbers() //? output: NaN (no argument that why)
+// addTwoNumbers(3,2) //? output: 5
+// addTwoNumbers(3,"2") //? output: 32 string
+// addTwoNumbers(3,"a") //? output: 3a string
 
+// function addTwoNumbers(number1, number2){ 
+//     console.log(number1 + number2);
+// }
+// console.log("Result: ", result); //? output: undefined if u not use return 
+
+//? correct way
 function addTwoNumbers(number1, number2){
-
     // let result = number1 + number2
     // return result
-    return number1 + number2
+    return number1 + number2  //? after return next line not execute, and store in varibale
 }
+const result = addTwoNumbers(3, 5) 
+// console.log("Result: ", result);  
 
-const result = addTwoNumbers(3, 5)
 
-// console.log("Result: ", result);
-
-// 2. when no value pass then outcome will undefined
+//! 2. when no value pass then outcome will undefined
 
 function loginUserMessage(username = "sam"){
     if(!username){
@@ -39,15 +48,23 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("hitesh"))
-// console.log(loginUserMessage())
+// console.log(loginUserMessage()) //? its return undefined
 
-// 3. REST operator
+//! 3. REST operator
+//? when we have many value to pass then we use REST Ops (...) 
+//* case 1
 function calculateCartPrice(...num1){
     return num1
 }
-// console.log(calculateCartPrice(200, 400, 500, 1000, 5000));
+// console.log(calculateCartPrice(200, 400, 500, 1000, 5000));  //? array output: [200, 400, 500, 1000, 5000]
 
-// 4. pass objects
+//* case 2
+function calculateCartPrice(val1, val2,...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 500, 1000, 5000)); //? output: [500, 1000, 5000]
+
+//! 4. pass objects
 const user = {
     username: 'Arup',
     price: 100
@@ -56,16 +73,16 @@ function handleObject(anyobject)
 {
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
-// handleObj(user)
+// handleObject(user) //? call object, sometimes we have some issue with type... eg: price become prices
+
+//? direct make function and pass object
 handleObject({
     username: "sam",
     price: 399
 })
 
-// 5. pass array
-
+//! 5. pass array
 const myNewArray = [200, 400, 100, 600]
-
 function returnSecondValue(getArray){
     return getArray[1]
 }
