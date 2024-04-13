fc9115eb0123b613e1a517d8f89198f14ef6b8b3
X-DX <aruproy5637@gmail.com>
2024-04-13 14:01:24 +0530

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,12 +1,24 @@
+//! {} this we call scope, when we used with function
 // let a=10;
 // const  b= 20
-//var c = 300
+// var c = 300
+
 // console.log(a);
 // console.log(b);
 // console.log(c);
 
-//  1. scope {}, globe scope and block scope
+//!  1. scope {}, globe scope and block scope
+//* CASE 1
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30 
+}
+// console.log(a); //? it will show error
+// console.log(b); //? it will show error
+// console.log(c); //? output: 30 why ? //
 
+//* CASE 2
 let a = 300
 if (true) {
     let a = 10
@@ -14,7 +26,7 @@ if (true) {
     // console.log("INNER: ", a);  // print 10 not 300 
 }
 
-//2. Nested Scope
+//! 2. Nested Scope
 
 function one(){
     const username = "hitesh"
@@ -24,7 +36,6 @@ function one(){
         console.log(username);
     }
     // console.log(website); // block scope error
-
      two() 
 }
 // one()
@@ -40,8 +51,8 @@ if (true) {
 
 // console.log(username); // same error block scope
 
-// ++++++++++++++++++ interesting ++++++++++++++++++
-//hosting concept
+//! ++++++++++++++++++ interesting ++++++++++++++++++
+//* hosting concept
 
 console.log(addone(5)) // its show output : 6, because here we normally declear a function
 
@@ -49,7 +60,7 @@ function addone(num){
     return num + 1
 }
 
-// addTwo(5) // show error , beacause here we store function value in varibale
+//* addTwo(5) // show error , beacause here we store function value in varibale
 
 const addTwo = function(num){ // here function store in varibale i.e we called experssion
     return num + 2
