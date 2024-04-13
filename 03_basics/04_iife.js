23159f6e7eff93c4e3c0f7de7278b225c4ad8a60
X-DX <aruproy5637@gmail.com>
2024-04-13 17:25:44 +0530

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,6 +1,10 @@
-// An Immediately Invoked Function Expression (IIFE) is a common JavaScript design pattern that involves defining and executing a function immediately after its creation. It is typically used to create a new scope for variables, preventing them from polluting the global scope.
+// An Immediately Invoked Function Expression (IIFE) is a common JavaScript design pattern that involves defining and 
+// executing a function immediately after its creation. It is typically used to create a new scope for variables, 
+// preventing them from polluting the global scope.
 
-// The primary benefits of using IIFE include avoiding global namespace pollution, creating private variables, and controlling variable scope. IIFE is commonly used in scenarios where you want to execute some code immediately without leaving variables or functions in the global scope that might interfere with other parts of your program.
+// The primary benefits of using IIFE include avoiding global namespace pollution, creating private variables, 
+// and controlling variable scope. IIFE is commonly used in scenarios where you want to execute some code immediately 
+// without leaving variables or functions in the global scope that might interfere with other parts of your program.
 
 // preventing them from polluting the global scope.
 (function chai(){
