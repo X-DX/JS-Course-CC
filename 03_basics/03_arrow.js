9713f61f51c4b78b131f6f94c4c7a7c1aea15cea
X-DX <aruproy5637@gmail.com>
2024-04-13 17:21:55 +0530

    arror function

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,12 +1,13 @@
-// arrror function
-// 1. this -> current context
+//! arrow function
+//! 1. this -> current context
+
 const user = {
     username: "hitest",
     price: 900,
 
     welcomeMessage: function() {
         console.log(`${this.username}, welcome to website`);
-        // console.log(this);  // tell about current context or current value
+        // console.log(this);  //* tell about current context or current value then we use this
     }
 }
 // user.welcomeMessage
@@ -16,11 +17,12 @@ const user = {
 
 // console.log(this);  // empty {}, node enironment 
 
-// In JavaScript, the behavior of this in the global context depends on where the code is executed. If the code is executed in the global scope, this refers to the global object, which is window in a browser environment.
+// In JavaScript, the behavior of this in the global context depends on where the code is executed. 
+// If the code is executed in the global scope, this refers to the global object, which is window in a browser environment.
 
-// However, when you use console.log(this) in certain environments like a browser's console, the output may appear as {} even though it represents the global object (window in a browser). This is a common representation used by some console environments to indicate the global object.
-
-
+// However, when you use console.log(this) in certain environments like a browser's console, the output may appear as {} even 
+//though it represents the global object (window in a browser). This is a common representation used by some console 
+//environments to indicate the global object.
 
 // function chai(){
 //     let username = "hitesh"
@@ -30,13 +32,15 @@ const user = {
 
 // chai()
 
-// 2. arrow function
+//? what is different diffrent between arror function and normal function
+
+//! 2. arrow function
 
 // const chai = function () {
 //     let username = "hitesh"
 //     console.log(this.username);
 // }
-// // chai()
+// chai()
 
 const chai =  () => {
     let username = "hitesh"
@@ -44,17 +48,18 @@ const chai =  () => {
 }
 // chai()
 
-// explesite return
+//? explesite return, if we {} we have to write return
 const addTwo = (num1, num2) => {
     return num1 + num2
 }
 
-//3. implesite return (use in react)
+//? implesite return (use in react) , we if use (), no need to write return
 
 // const addTwo = (num1, num2) =>  num1 + num2
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
+//? object return, we have use ()
 // const addTwo = (num1, num2) => ({username: "hitesh"})
 
 
