b6cdfacce465f9066bfb7c2fea09326952e1723b
X-DX <aruproy5637@gmail.com>
2024-07-13 04:24:14 +0530

    currency converter

---
+++ 07_projects/7-currencyExchange/script.js
@@ -0,0 +1,9 @@
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+// console.log(countryCode);
+
+for(countryCode in countryList)
+{
+    console.log(countryCode);
+}
