514dc526d4a3b355be9fea7c4491715db9673a8b
X-DX <aruproy5637@gmail.com>
2024-07-13 12:52:26 +0530

    currency

--- 07_projects/7-currencyExchange/script.js
+++ 07_projects/7-currencyExchange/script.js
@@ -1,9 +1,54 @@
-const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+const apiKey = "453b43b410f75e41ff0c1b15";
+const BASE_URL = `https://v6.exchangerate-api.com/v6/${apiKey}/enriched`;
 
 const dropdowns = document.querySelectorAll(".dropdown select");
-// console.log(countryCode);
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
 
-for(countryCode in countryList)
-{
-    console.log(countryCode);
+// console.log(countryCode);
+let i = 0;
+for(let select of dropdowns){
+     for(currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if(select.name === "from" && currCode === "USD"){
+            newOption.selected = "selected";
+        }
+
+        if(select.name === "to" && currCode === "INR"){
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+     }
+     select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+     })
 }
+
+const updateFlag = (element) => {
+    // console.log(element);
+    let currCodeChange = element.value;
+    let countryCodeChange = countryList[currCodeChange]; 
+    let newSrc = `https://flagsapi.com/${countryCodeChange}/flat/64.png`;
+
+    let img = element.parentElement.querySelector('img');
+    img.src = newSrc;
+}
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtvalue = amount.value;
+    if(amtvalue === '' || amtvalue < 1)
+    {
+         amtvalue = 1;
+         amount.value = "1";
+    }
+
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()]
+});
