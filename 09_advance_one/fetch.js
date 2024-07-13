bc232cd585f4e10d0352a94555d86a867a6492b0
X-DX <aruproy5637@gmail.com>
2024-07-13 16:34:33 +0530

    fetch

--- 09_advance_one/fetch.js
+++ 09_advance_one/fetch.js
@@ -1,5 +1,10 @@
 fetch('https://jsonplaceholder.typicode.com/users')
 
-// how this fetch works ?
+//* how this fetch works ?
 // https://blog.logrocket.com/fetch-api-node-js/
 // https://developer.mozilla.org/en-US/docs/Web/API/fetch
+
+//* The Fetch standard defines requests, responses, and the process that binds them: fetching.
+// https://fetch.spec.whatwg.org/
+
+
